2fd569723aabb4f7f88d60f60db6bb29ff7f4b70
Chinh Le <chinh.le11@gmail.com>
2020-03-27 11:19:00 -0400

    Add CSS transition

--- src/CONST.js
+++ src/CONST.js
@@ -28,6 +28,34 @@ export const QUOTES = [
       author: "― Blaise Pascal, Pascal's Pensees"
     },
     {
+      text: "All animals are equal, but some are more equal than others.",
+      author: "― George Orwell"
+    },
+    {
+      text: "I must be cruel only to be kind.",
+      author: "― William Shakespeare"
+    },
+    {
+      text: "I close my eyes so I can see.",
+      author: "― Fugazi, Shut the Door"
+    },
+    {
+      text: "Whatever you do will be insignificant, but it is very important that you do it.",
+      author: "― Gandhi"
+    },
+    {
+      text: "Life is a preparation for the future; and the best preparation for the future is to live as if there were none.",
+      author: "― Albert Einstein"
+    },
+    {
+      text: "I know one thing: that I know nothing.",
+      author: "― Socrates"
+    },
+    {
+      text: "I can resist everything except temptation.",
+      author: "― Oscar Wilde"
+    },
+    {
       text: "And the trouble is, if you don't risk anything, you risk even more.",
       author: "― Erica Jong"
     }
--- src/components/QuoteComponent.js
+++ src/components/QuoteComponent.js
@@ -5,6 +5,7 @@ import GoogleFontLoader from 'react-google-font-loader';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuoteRight, faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
 import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { CSSTransition } from 'react-transition-group';
 
 import './QuoteComponent.scss';
 import { QUOTES } from '../CONST';
@@ -23,36 +24,68 @@ class QuoteComponent extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      quote: {}
+      quote: {},
+      fade: true
     };
     this.interval = null;
+    this.timeout = null;
     this.getQuote = this.getQuote.bind(this);
+    this.changeQuote = this.changeQuote.bind(this);
+    // this.wrapper = React.createRef();
   }
 
   componentDidMount(){
-    this.setState({
-        quote: getRandomQuote()
+    this.setState((prevState, props) => {
+        return {
+          quote: getRandomQuote(),
+          fade: prevState.fade
+        };
     });
     this.setRandomQuote();
   }
 
+  componentWillUnmount(){
+    clearInterval(this.interval);
+    clearTimeout(this.timeout);
+  }
+
   setRandomQuote(){
     this.interval = setInterval(() => {
-      this.setState({
-        quote: getRandomQuote()
+      this.setState((prevState, props) => {
+        return {
+          quote: prevState.quote,//getRandomQuote(),
+          fade: !prevState.fade
+        };
       });
+      this.changeQuote();
     },5000);
+  }
+
+  changeQuote(){
+    this.timeout = setTimeout(() => {
+      this.setState((prevState, props) => {
+        return {
+          quote: getRandomQuote(),
+          fade: !prevState.fade
+        };
+      });
+    }, 500);
   }
 
   getQuote(){
     clearInterval(this.interval);
-    this.setState({
-       quote: getRandomQuote()
+    this.setState((prevState, props) => {
+       return {
+         quote: prevState.quote,//getRandomQuote(),
+         fade: !prevState.fade
+       };
     });
-  }
+    this.changeQuote();
+  }    
 
   render(){
     return(
+      // <div ref={this.wrapper}>
       <div>
         <GoogleFontLoader
           fonts={[
@@ -63,20 +96,26 @@ class QuoteComponent extends React.Component {
           ]}
         />
         <Card bg="transparent" border="0" className="rnd-quote p-4">
-            <Card.Body className="rnd-quote__body d-flex flex-column justify-content-center">
-              <Card.Text className="rnd-quote__text mx-auto text-light m-0"><FontAwesomeIcon icon={faQuoteLeft} /></Card.Text>
+          <Card.Body className="rnd-quote__body d-flex flex-column justify-content-center">
+            <Card.Text className="rnd-quote__text mx-auto text-light m-0"><FontAwesomeIcon icon={faQuoteLeft} /></Card.Text>
               <Card.Title className="rnd-quote__title d-flex flex-row align-items-center mx-auto mb-0">
-                {this.state.quote.text}
+                <CSSTransition in={this.state.fade} timeout={500} className="rnd-quote__title--fade">
+                  <span>{this.state.quote.text}</span>
+                </CSSTransition>
               </Card.Title>
-              <Card.Text className="rnd-quote__text mx-auto text-right text-light"><FontAwesomeIcon icon={faQuoteRight} /></Card.Text>
-            </Card.Body>
-            <Card.Footer className="rnd-quote__footer mx-auto bg-transparent border-0">
-              <Card.Text className="d-flex flex-column align-items-end text-light">{this.state.quote.author}</Card.Text>
-            </Card.Footer>
-            <Card.Footer className="rnd-quote__footer d-flex justify-content-center mx-auto bg-transparent border-0">
-                <Button variant="light" className="mx-1" id="tweet-quote"><FontAwesomeIcon icon={faTwitter} /></Button>
-                <Button variant="light" id="new-quote" onClick={this.getQuote}>New Quote</Button>
-            </Card.Footer>
+            <Card.Text className="rnd-quote__text mx-auto text-right text-light"><FontAwesomeIcon icon={faQuoteRight} /></Card.Text>
+          </Card.Body>
+          <Card.Footer className="rnd-quote__footer mx-auto bg-transparent border-0">
+            <Card.Text className="d-flex flex-column align-items-end text-light">
+              <CSSTransition in={this.state.fade} timeout={200} className="rnd-quote__author--fade">
+                <span>{this.state.quote.author}</span>
+              </CSSTransition>
+            </Card.Text>
+          </Card.Footer>
+          <Card.Footer className="rnd-quote__footer d-flex justify-content-center mx-auto bg-transparent border-0">
+            <Button variant="light" className="mx-1" id="tweet-quote"><FontAwesomeIcon icon={faTwitter} /></Button>
+            <Button variant="light" id="new-quote" onClick={this.getQuote}>New Quote</Button>
+          </Card.Footer>
         </Card>
       </div>
     )
